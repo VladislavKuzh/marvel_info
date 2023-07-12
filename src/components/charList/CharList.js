5d5eeee1fff3d6473a9b10f83caacab2184b0f32
VladislavKuzh <v.s.kuzheliev@gmail.com>
2023-07-12 20:15:39 +0300

    Сhanged focus method

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -48,7 +48,8 @@ class CharList extends Component {
             loading: false,
             newItemLoading: false,
             offset: offset + 9,
-            charEnded: ended
+            charEnded: ended,
+            characterSelected: null
         }))
     }
 
@@ -59,20 +60,11 @@ class CharList extends Component {
         })
     }
 
-    itemRefs = [];
-
-    setRef = (ref) => {
-        this.itemRefs.push(ref);
-    }
-
-    focusOnItem = (id) => {
-        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
-        this.itemRefs[id].classList.add('char__item_selected');
-        this.itemRefs[id].focus();
-    }
-
     renderItems(arr) {
         const items = arr.map((item, i) => {
+
+            const active = this.state.characterSelected === item.id
+            const clazz = active ? 'char__item char__item_selected' : 'char__item';  
 
             let imgStyle = {'objectFit' : 'cover'};
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
@@ -80,13 +72,12 @@ class CharList extends Component {
             }
         
             return (
-                <li className="char__item"
+                <li className={clazz}
                     tabIndex={0}
-                    ref={this.setRef}
                     key={item.id}
                     onClick={() => {
                         this.props.onCharSelected(item.id);
-                        this.focusOnItem(i);}}>
+                        this.setState({characterSelected: item.id})}}>
                     <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                     <div className="char__name">{item.name}</div>
                 </li>
