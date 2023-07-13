9d0b04f38f60bd8506955112bc68836a838f3e97
VladislavKuzh <v.s.kuzheliev@gmail.com>
2023-07-13 17:16:05 +0300

    Homework. Added comics section

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -22,7 +22,7 @@ const RandomChar = () => {
 
     const updateChar = () => {
         clearError();
-        const id = Math.floor(Math.random() * (1010000 - 1009146) + 1009146);
+        const id = Math.floor(Math.random() * (1011400 - 1011000)) + 1011000;
         getCharacter(id)
             .then(onCharLoaded);
     }
